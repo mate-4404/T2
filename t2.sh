e19d8cfca4170957eaf0ebf73556312c5255b027
mate <115540974+mate-4404@users.noreply.github.com>
2023-03-23 10:46:32 +0530

    Update t2.sh

--- t2.sh
+++ t2.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
-FILE="filename.txt"
-if [ ! -f "$FILE" ]; then
-    echo "File does not exist. Creating file..."
-    echo "hello world" > "$FILE"
+read FILE
+echo "enter your file name :"
+
+
+if [ ! -f "$FILE" ];                          //	When file runs, a variable will take response as yes or no.
+then
+    echo "File does not exist. Want to Create a file enter y/n"
+    read create
+    if $create = y
+    echo "Anne mangoes 7
+Betty limes 14
+Fred apples 20
+Greg pineapples 3
+Lisa peaches 7
+Mark grapes 39
+Mark watermellons 12
+Oliver rockmellons 2
+Robert pears 4
+Susy oranges 12
+Susy oranges 5
+Terry oranges 9 " > "$FILE"
+
     echo "File created and 'hello world' added."
+    then echo "as per your choice file will not be created"
 else
     echo "File already exists."
 fi
