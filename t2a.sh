eec3bc11b0e66cc0b2fb53729970f12b4eea7e5c
mate <115540974+mate-4404@users.noreply.github.com>
2023-03-23 12:46:06 +0530

    Update t2a.sh

--- t2a.sh
+++ t2a.sh
@@ -22,3 +22,31 @@ if [ -f "$filename" ]; then
 else
     echo "The file '$filename' does not exist."
 fi
+
+
+
+read -p "Do you have a file? (yes/no): " response
+
+if [ "$response" = "yes" ]; then
+    read -p "Enter the name of the file: " filename
+else
+    read -p "Do you want to create a file? (yes/no): " create_response
+
+    if [ "$create_response" = "yes" ]; then
+        read -p "Enter the name of the file: " filename
+        read -p "Enter data in the file: " data
+
+        echo "$data" > $filename
+        touch "$filename"
+    else
+        echo "Exiting script."
+        exit 0
+    fi
+fi
+
+if [ -f "$filename" ]; then
+    num_lines=$(wc -l < "$filename")
+    echo "The file '$filename' has $num_lines lines."
+else
+    echo "The file '$filename' does not exist."
+fi
